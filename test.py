df282c253a9e8a3497a8f7a94dc27875e1dc2c12
nutahiroba <nutaba.hiroto.qd@alumni.tsukuba.ac.jp>
2023-10-24 12:34:14 +0900

    gitignoreを変更

--- test.py
+++ test.py
@@ -1,22 +1,36 @@
 import pass2txt
 import count_noun
 from nltk.corpus import stopwords
+import nltk
 
-stoppath = "Japanese.txt"
-with open(stoppath, "r", encoding= "utf-8") as f:
+
+with open("stopword.txt", "r",encoding="utf-8") as f:
   stopwords = f.readlines()
-
-# nltkモジュールから取得したstopwords
-with open("English.txt", "r", encoding="utf-8") as f:
-  en_stopwords = f.readlines()
-
-en_sw = en_stopwords[0].replace(",|[|]| ", "")
-for word in en_sw.split(","):
-  stopwords.append(word[2:-1])
 
 stopwords = [string.strip() for string in stopwords if string.strip()]
 
-print(stopwords)
+for word in stopwords:
+  print(word)
+
+# nltk.download('stopwords')
+# stop_words = stopwords.words('english')
+# print(stop_words)
+
+# stoppath = "Japanese.txt"
+# with open(stoppath, "r", encoding= "utf-8") as f:
+#   stopwords = f.readlines()
+
+# # nltkモジュールから取得したstopwords
+# with open("English.txt", "r", encoding="utf-8") as f:
+#   en_stopwords = f.readlines()
+
+# en_sw = en_stopwords[0].replace(",|[|]| ", "")
+# for word in en_sw.split(","):
+#   stopwords.append(word[2:-1])
+
+# stopwords = [string.strip() for string in stopwords if string.strip()]
+
+# print(stopwords)
 
 # p = r"C:\Users\nutta\OneDrive\ドキュメント\授業資料\(学類長・専門学群長宛)平成31年度座長団選出報告書.docx"
 
