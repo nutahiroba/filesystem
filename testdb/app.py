d7367aab866480126713f0ee8a9ed2baa1a662ec
nutahiroba <nutaba.hiroto.qd@alumni.tsukuba.ac.jp>
2023-11-07 19:29:03 +0900

    testdbに追加、削除機能を追加

--- testdb/app.py
+++ testdb/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,14 +8,31 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class ToDo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    todo = db.Column(db.String(128), nullable=False)
+	id = db.Column(db.Integer, primary_key=True)
+	todo = db.Column(db.String(128), nullable=False)
 
 @app.route('/')
 def index():
-    return render_template('todo.html')
+	data = ToDo.query.all()
+	print(data)
+	return render_template('todo.html',data=data)
+	
+@app.route('/add', methods=['POST'])
+def add():
+	todo = request.form['todo']
+	new_todo = ToDo(todo=todo)
+	db.session.add(new_todo)
+	db.session.commit()
+	return redirect(url_for('index'))
+	
+@app.route('/del_todo/<int:id>')
+def del_todo(id):
+	del_data = ToDo.query.filter_by(id=id).first()
+	db.session.delete(del_data)
+	db.session.commit()
+	return redirect(url_for('index'))
+	
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run()
+	#db.create_all()
+	app.run()
--- testdb/config.py
+++
@@ -1,3 +0,0 @@
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///sample_flask.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
